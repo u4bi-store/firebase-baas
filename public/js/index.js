5a69dd9dafd2e2c0578dc0899fdb8560ac6810a8
u4bi <u4bi@u4bi.com>
2017-02-08 11:50:59 +0900

    firebase auth, crud repack

--- public/js/index.js
+++ public/js/index.js
@@ -8,111 +8,79 @@ var config = {
     messagingSenderId: "67758349179"
 };
 
-firebase.initializeApp(config);
-auth = firebase.auth();
-database = firebase.database();
+init();
 
-var authProvider = new firebase.auth.GoogleAuthProvider();
-auth.onAuthStateChanged(function(user) {
-    if (user) {
-    //signed
-    userInfo = user;
-    console.log('success', user);
-    getMemoList();
-    } else {
-    // denied
-    auth.signInWithPopup(authProvider);
-    }
-});
+function init(){
+    firebase.initializeApp(config);
+    auth = firebase.auth();
+    database = firebase.database();
 
-function getMemoList() {
+    var authProvider = new firebase.auth.GoogleAuthProvider();
+
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            // 회원가입
+            userInfo = user;
+            console.log(' success', user);
+            selects();
+        } else {
+            // 재로그인
+            auth.signInWithPopup(authProvider);
+        }
+    });
+}
+
+// selects
+// select
+// save
+// remove
+
+function selects(){
     var memoRef = database.ref('memos/' + userInfo.uid);
     memoRef.on('child_added', onChildAdded);
     memoRef.on('child_changed', onChildChanged);
 }
-
-function onChildChanged(data) {
-    $('#'+data.key).replaceWith(getHtml(data));
-}
-
-function onChildAdded(data) {
-        $('.collection').append(getHtml(data));
-}
-
-function getHtml(data) {
-    var key = data.key;
-    var memo = data.val();
-    console.log('childData', key);
-    console.log('childData', memo);
-    var txt = memo.txt;
-    var title = txt.substr(0, 3);
-    var firstTxt = txt.substr(0,1);
-    return "<li id='"+key+"' class=\"collection-item avatar\" onclick=\"getMemo(this.id);\" >" +
-        "<i class=\"material-icons circle red\">" +firstTxt + "</i>" +
-        "<span class=\"title\">" + title + "</span>" +
-        "<p class='txt'>" + txt + "<br>" +
-        "</p>" +
-        "<a href=\"#\" onclick=\"deleteMemo('"+ key +"')\">삭제</a>" +
-        "</li>";
-
-}
-
-function getMemo(key) {
+function select(key) {
     selectedKey = key;
     var memoRef = database.ref('memos/' + userInfo.uid + '/' + key)
     .once('value')
-    .then(function(snapshot) {
-        $('.textarea').val(snapshot.val().txt);
+    .then(function(data) {
+        console.log(data);
+        console.log(' select -------------------------');
     });
 }
-
-function saveMemo() {
+function save() {
     var memoRef = database.ref('memos/' + userInfo.uid);
-    var txt = $('.textarea').val();
-    if (!txt) {
-    return;
-    }
+    var txt = 'tset123';
 
     if(selectedKey) {
-    var memoRef2 = database.ref('memos/' + userInfo.uid + '/' + selectedKey);
-    memoRef2.update({
-        txt: txt,
-        updateDate: new Date().getTime()
-    })
+        var memoRef2 = database.ref('memos/' + userInfo.uid + '/' + selectedKey);
+        memoRef2.update({
+            txt: txt,
+            updateDate: new Date().getTime()
+        });
     } else {
-    memoRef.push({
-        txt: txt,
-        createDate: new Date().getTime()
-    })
+        memoRef.push({
+            txt: txt,
+            createDate: new Date().getTime()
+        });
     }
 }
 
-function initMemo() {
-    $('.textarea').val('');
-    selectedKey = undefined;
-}
-
-function deleteMemo(key) {
-    if (!confirm('삭제하시겠습니까?')) {
-    return;
-    } else {
-    var memoRef = database.ref('memos/' + userInfo.uid + '/' + key);
+function remove(key) {
+    var memoRef = database.ref('memos/' + userInfo.uid + '/' + key);        
+    console.log(memoRef);
+    console.log(' remove -------------------------');
     memoRef.remove();
-    $('#'+key).remove();
-    }
 }
 
-$(function() {
-    $('.textarea').blur(function() {
-    saveMemo();
-    });
-});
-/*
-    {
-        memos :{
-        uid : { text : '텍스트', 작성일:  '작성일', 제목 : '제목'},
-        uid : { text : '텍스트', 작성일:  '작성일', 제목 : '제목'},
-        uid : { text : '텍스트', 작성일:  '작성일', 제목 : '제목'},
-        }
-    }
-*/
+
+function onChildChanged(data) {
+    console.log(data);
+    console.log(' changed -------------------------');
+}
+
+function onChildAdded(data) {
+    console.log(data.val());
+    console.log(' added -------------------------');
+}
