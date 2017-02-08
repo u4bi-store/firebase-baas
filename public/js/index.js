5242ee448c9714c2827a10f642dddb411b9105fa
u4bi <u4bi@u4bi.com>
2017-02-08 14:09:58 +0900

    save, update , itemId array push render

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 var auth, database, userInfo, selectedKey;
 
+var itemArray = [];
+
 var config = {
     apiKey: "AIzaSyBI8zqLEQsKI4be9Zm4xmrPRNhCNUV_IhU",
     authDomain: "memoapp-dd157.firebaseapp.com",
@@ -21,10 +23,12 @@ function init(){
         if (user) {
             // 회원가입
             userInfo = user;
-            console.log('인증완료 : ', user);
+            console.log('인증완료 : ', userInfo);
             
-            authInfo(user); /* 인증 유저의 제공 사이트 정보조회*/
+            authInfo(); /* 인증 유저의 제공 사이트 정보조회*/
             selects(); /* 모든 유저를 조회함 */
+            
+            save('test기록'); /* 유저 저장*/
         } else {
             // 재로그인
             auth.signInWithPopup(authProvider);
@@ -43,7 +47,6 @@ function selects(){
     memoRef.on('child_changed', onChildChanged);
 }
 function select(key) {
-    selectedKey = key;
     var memoRef = database.ref('memos/' + userInfo.uid + '/' + key)
     .once('value')
     .then(function(data) {
@@ -51,21 +54,29 @@ function select(key) {
         console.log(' select -------------------------');
     });
 }
-function save() {
+function save(str, selectedKey = undefined) {
     var memoRef = database.ref('memos/' + userInfo.uid);
-    var txt = 'tset123';
+    var date = new Date().getTime();
 
     if(selectedKey) {
         var memoRef2 = database.ref('memos/' + userInfo.uid + '/' + selectedKey);
         memoRef2.update({
-            txt: txt,
-            updateDate: new Date().getTime()
+            txt: str,
+            updateDate: date
         });
-    } else {
+        
+        /* save('변경함',itemArray[21].itemId); */
+        console.log('[업데이트] 내용 : '+str+' 저장시간 : '+date);
+        selectedKey = undefined;
+
+    }else{
         memoRef.push({
-            txt: txt,
-            createDate: new Date().getTime()
+            txt: str,
+            createDate: date
         });
+        
+        /* save('변경함'); */
+        console.log('[저장완료] 내용 : '+str+' 저장시간 : '+date);
     }
 }
 
@@ -87,18 +98,13 @@ function onChildAdded(data) {
     var json = data.val();
 
     var txt = json.txt;
-    var date = json.createDate;
-
-    // console.log('유저의 키코드 : '+key);
-    // console.log('내용 : '+txt);
-    // console.log('기록시간 : '+date);
-    // console.log('----------------------');
-
+    var date = json.createDate;    
+    itemArray.push({itemId : key});
 }
 
 /* 인증 유저의 제공 사이트 프로필 정보조회*/
-function authInfo(user){
-    user.providerData.forEach(function (profile) {
+function authInfo(){
+    userInfo.providerData.forEach(function (profile) {
         console.log("제공 사이트: "+profile.providerId);
         console.log("유저 고유번호 : "+profile.uid);
         console.log("유저 이름 : "+profile.displayName);
@@ -107,30 +113,30 @@ function authInfo(user){
     });
     console.log('-----------------------------------');
     
-    profileInfo(user); /* 변경전 인증 유저의 사용자 프로필 조회 */
-    authProfileUpdate(user);/* 인증 유저의 사용자 프로필 업데이트 */
+    profileInfo(); /* 변경전 인증 유저의 사용자 프로필 조회 */
+    authProfileUpdate();/* 인증 유저의 사용자 프로필 업데이트 */
 }
 
 /* 인증 유저 프로필 업데이트*/
-function authProfileUpdate(user){
-    user.updateProfile({
+function authProfileUpdate(){
+    userInfo.updateProfile({
         displayName: "닉네임 변경함",
         photoURL: "https://example.com/jane-q-user/profile.jpg"
     }).then(function() {
         console.log('인증 유저 프로필 업데이트 성공');
-        profileInfo(user); /* 변경후 인증 유저의 사용자 프로필 조회 */
+        profileInfo(); /* 변경후 인증 유저의 사용자 프로필 조회 */
     }, function(error) {
         console.log(error); /* 에러 송신부 */
     });
     console.log('-----------------------------------');
 }
 
-function profileInfo(user){
+function profileInfo(){
 
-    var uid = user.uid;
-    var name = user.displayName;
-    var email = user.email;
-    var photo = user.photoURL;
+    var uid = userInfo.uid;
+    var name = userInfo.displayName;
+    var email = userInfo.email;
+    var photo = userInfo.photoURL;
 
     console.log('유저 고유번호 : '+uid);
     console.log('유저이름 : '+name);
